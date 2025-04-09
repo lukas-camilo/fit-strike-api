07fac98e222bdf6e2810211bc51d86a14e903f93
lukas-camilo <lukscamilo@gmail.com>
2025-04-09 19:08:04 -0300

    Ajustando base

---
+++ app/lambda_function.py
@@ -0,0 +1,21 @@
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+from aws_lambda_powertools.logging import Logger
+from aws_lambda_powertools.tracing import Tracer
+from aws_lambda_powertools.metrics import Metrics
+
+from src.interface_adapters.controllers.product_controller import register_product_routes
+
+# Initialize Powertools utilities
+logger = Logger()
+tracer = Tracer()
+metrics = Metrics()
+app = APIGatewayRestResolver()
+
+# Register routes from controllers
+register_product_routes(app)
+
+@logger.inject_lambda_context
+@tracer.capture_lambda_handler
+def lambda_handler(event, context):
+    """Main Lambda handler"""
+    return app.resolve(event, context)
--- src/test.py
+++
@@ -1,8 +0,0 @@
-from aws_lambda_powertools.event_handler import APIGatewayRestResolver
-
-from interface_adapters.controllers.product_controller import register_product_routes
-
-app = APIGatewayRestResolver()
-
-a = register_product_routes(app).list_products_handler()
-print(f"Aqui => {a}")
