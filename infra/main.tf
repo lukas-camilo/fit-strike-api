2f0b9671214df1c2cd5c137afddf9a4c3575bedb
lukas-camilo <lukscamilo@gmail.com>
2025-04-11 14:52:18 -0300

    Ajustando terraform

--- infra/main.tf
+++ infra/main.tf
@@ -41,7 +41,7 @@ resource "aws_lambda_function" "my_lambda" {
   role             = aws_iam_role.lambda_role.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.9"
-  file_name        = "../lambda_function.zip"
+  filename        = "../lambda_function.zip"
 
   environment {
     variables = {
