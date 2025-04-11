e216743c9c63226ed0a1b64d726ff5fc0fd52194
lukas-camilo <lukscamilo@gmail.com>
2025-04-11 14:50:52 -0300

    Ajustando variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "Região AWS"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "lambda_name" {
   description = "Nome da função lambda"
   type        = string
