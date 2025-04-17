576f8a129e16fbd70d3da97f3c1d7afca632f20e
lukas-camilo <lukscamilo@gmail.com>
2025-04-17 16:58:27 -0300

    Ajustando controller

--- app/src/interface_adapters/controllers/user_controller.py
+++ app/src/interface_adapters/controllers/user_controller.py
@@ -4,7 +4,7 @@ from aws_lambda_powertools import Logger, Tracer, Metrics
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver, Response
 from aws_lambda_powertools.metrics import MetricUnit
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
-from aws_lambda_powertools.utilities.validation import validate
+from aws_lambda_powertools.utilities.validation import validate, SchemaValidationError
 
 from src.application.use_cases.user.add_user import AddUserUseCase
 from src.application.use_cases.user.get_users import GetUsersUseCase
@@ -37,20 +37,33 @@ def register_user_routes(app: APIGatewayRestResolver):
     @tracer.capture_method
     @app.post("/users")
     def create_user():
-        event: APIGatewayProxyEvent = app.current_event
-        data = event.json_body
-        validate(event=data, schema=create_user_schema)
+        try:
+            event: APIGatewayProxyEvent = app.current_event
+            data = event.json_body
+            validate(event=data, schema=create_user_schema)
 
-        add_user_use_case.execute(str(uuid.uuid4()), data['name'], data['email'])
+            add_user_use_case.execute(str(uuid.uuid4()), data['name'], data['email'])
 
-        metrics.add_metric(name="UserCreated", unit=MetricUnit.Count, value=1)
-        return Response(
-            status_code=201,
-            content_type="application/json",
-            body={
-                "message": "User created successfully"
-            }
-        )
+            metrics.add_metric(name="UserCreated", unit=MetricUnit.Count, value=1)
+            
+            return Response(
+                status_code=201,
+                content_type="application/json",
+                body={
+                    "message": "User created successfully"
+                }
+            )
+        except SchemaValidationError as e:
+            missing_fields = [error.path for error in e.context]
+            return Response(
+                status_code=400,
+                content_type="application/json",
+                body={
+                    "message": "Invalid input data",
+                    "missing_fields": missing_fields,
+                    "error": str(e)
+                }
+            )
 
     @tracer.capture_method
     @app.get("/users")
